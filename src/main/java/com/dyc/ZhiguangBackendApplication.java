aafecd563e5618cc4f148f9b8952ae6aff7808ec
duoladeAmeng <1107128204@qq.com>
2026-03-05 17:22:44 +0800

    first commit

---
+++ src/main/java/com/dyc/ZhiguangBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhiguangBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhiguangBackendApplication.class, args);
+    }
+
+}
